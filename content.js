d13577202a8175107974359fb98f755032556979
Devraj Mehta <devm33@github.com>
2026-01-25 14:05:17 -0500

    align

--- content.js
+++ content.js
@@ -94,10 +94,10 @@
     button.id = 'copilot-request-review-btn';
     button.className = 'Button Button--iconOnly Button--secondary Button--small';
     button.type = 'button';
-    button.style.cssText = 'margin-left: 4px; position: relative; top: -4px;';
+    button.style.cssText = 'margin-left: 4px; padding-right: 1px; position: relative; top: -4px;';
     button.title = 'Request Copilot review for draft PR';
     const playIcon = `
-      <svg aria-hidden="true" height="16" viewBox="0 0 16 16" width="16" class="octicon">
+      <svg aria-hidden="true" height="16" viewBox="0 0 16 16" width="16" class="octicon" style="position: relative; left: 1px;">
         <path fill="currentColor" d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0ZM1.5 8a6.5 6.5 0 1 0 13 0 6.5 6.5 0 0 0-13 0Zm4.879-2.773 4.264 2.559a.25.25 0 0 1 0 .428l-4.264 2.559A.25.25 0 0 1 6 10.559V5.442a.25.25 0 0 1 .379-.215Z"></path>
       </svg>
     `;
